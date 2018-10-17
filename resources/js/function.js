6596928ed03e1eabafc8507595673d3c94007c1a
unknown <phanhonghai97@gmail.com>
2018-10-17 22:05:45 +0700

    update banner

--- resources/js/function.js
+++ resources/js/function.js
@@ -1,107 +1,49 @@
-/*datepicker*/
- $( function() {
-   $('.checkIn').datepicker({
-    dateFormat: 'yy-m-d',
-    inline: true,
-    onSelect: function(dateText, inst) { 
-        var date = $(this).datepicker('getDate'),
-            day  = date.getDate(),  
-            month = date.getMonth() + 1,
-            year = date.getFullYear();              
-        $('.checkIn').val(day); 	
-      	$('.monthCI').val(month+'.'+year);
-    }
+/* move slide Image*/
+var value=0;
+var x;
+var urlImg;
+var count=0;
+$(function(){
+	$('.nutRight').click(function(){
+		var getBanner=document.getElementById("banner");
+
+		if(count!=3){
+			count++;
+			urlImg="url(resources/images/home_bg"+count+".jpg) center center / cover no-repeat";
+			$('#banner').css('background',urlImg);
+			console.log(count);
+		}
+		else if(count == 3){
+			count=0;
+			urlImg="url(resources/images/home_bg.jpg) center center / cover no-repeat";
+			$('#banner').css('background',urlImg);
+			console.log(count);
+		}
+		$('#banner').css('background',);
+		if(value != -900){
+			value += -300;
+			x= "translateX("+value+"px)";
+			$('.slideChild').css('transform',x);
+		}
+		else{
+			value=0;
+			x="translateX("+value+"px)";
+			$('.slideChild').css('transform',x);
+		}
+	})
 });
-    $('.checkOut').datepicker({
-    dateFormat: 'yy-m-d',
-    inline: true,
-    onSelect: function(dateText, inst) { 
-        var date = $(this).datepicker('getDate'),
-            day  = date.getDate(),  
-            month = date.getMonth() + 1,
-            year = date.getFullYear();              
-        $('.checkOut').val(day); 	
-      	$('.monthCO').val(month+'.'+year);
-    }
+$(function(){
+	$('.nutLeft').click(function(){
+		if(value != 0){
+			value += 300;
+			x= "translateX("+value+"px)";
+			$('.slideChild').css('transform',x);
+		}
+		else{
+			value=-900;
+			x="translateX("+value+"px)";
+			$('.slideChild').css('transform',x);
+		}
+	})
 });
-   
-  } );
-/*end*/
-  
-/* start slide show  */
-
-var slideIndex = 1;
-var ktShow=0;
-showSlides(1);
-// Next/previous controls
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
-// Thumbnail image controls
-function currentSlide(n) {
-	showSlides(slideIndex = n);
-}
-function showSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("bannerchild");
-	var getCap = document.getElementsByClassName("cap");
-	console.log(slides.length);
-	var dots = document.getElementsByClassName("dot-child");
-	if (n > slides.length) {slideIndex = 1} 
-		if (n < 1) {slideIndex = slides.length}
-			for (i = 0; i < dots.length; i++) {
-				dots[i].className = dots[i].className.replace(" active", "");
-			}
-			for (i = 0; i < slides.length; i++) {
-				slides[i].className=slides[i].className.replace(" nghiengVe","");
-				slides[i].className = slides[i].className.replace(" kichHoat", "");
-				slides[i].className = slides[i].className.replace(" moveLeave", "");
-				slides[i].className = slides[i].className.replace(" moveNext", "");
-			}
-			for(i=0;i<getCap.length;i++){
-				getCap[i].className=getCap[i].className.replace(" capActive","");
-			}
-			slides[slideIndex-1].className += " kichHoat"; 
-			dots[slideIndex-1].className += " active";
-			getCap[slideIndex-1].className += " capActive";
-			if(slideIndex == 1){
-				slides[slides.length -1].className += " moveLeave"; 
-				slides[slideIndex-1].className += " moveNext";
-			}
-			else{
-				slides[slideIndex-2].className += " moveLeave"; 
-				slides[slideIndex-1].className += " moveNext"; 
-			}
-		}
-/* end slide show */
-
-/* show subMenu*/
-
-function showMenu(n){
-	console.log(ktShow);
-	var temp;
-	var getSubMenu =document.getElementsByClassName("subMenu");
-	var slides = document.getElementsByClassName("bannerchild");
-	if(ktShow==1){
-		for (var i =0; i < slides.length; i++) {
-			  slides[i].className=slides[i].className.replace(" nghiengTheoY","");
-			  slides[i].className += " nghiengVe";
-		}
-		getSubMenu[0].className=getSubMenu[0].className.replace(" subMenuShow","");
-		getSubMenu[0].className += " subMenuHide";
-		temp = 0;
-		ktShow=temp;
-	}
-	else if(ktShow == 0){
-		for (var i =0; i < slides.length; i++) {
-			slides[i].className=slides[i].className.replace(" nghiengVe","");
-			 slides[i].className += " nghiengTheoY";
-		}
-		getSubMenu[0].className=getSubMenu[0].className.replace(" subMenuHide","");
-		getSubMenu[0].className += " subMenuShow";
-		temp =1;
-		ktShow=temp;
-	}
-	console.log(ktShow);
-}
-/*end show subMenu*/
+/* end move slide Image*/
