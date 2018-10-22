c877dbcf68c3b7b02ce89acc06fbaabfea35b201
unknown <phanhonghai97@gmail.com>
2018-10-22 10:55:13 +0700

    up angular

--- resources/js/functionAngular.js
+++ resources/js/functionAngular.js
@@ -1,4 +1,8 @@
  var app = angular.module('myApp',['ngMaterial']);
  app.controller('MyController',  function($scope){
-  
+ 	$scope.hienThi =false;
+ 	$scope.showEdit=function (){
+ 		$scope.hienThi = !$scope.hienThi;
+ 	}
+
  })
