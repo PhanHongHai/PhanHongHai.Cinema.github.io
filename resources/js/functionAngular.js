2eda729d0c3d0918317b4c2fddf46237b1d0df0a
unknown <phanhonghai97@gmail.com>
2018-10-24 13:28:51 +0700

    ..

--- resources/js/functionAngular.js
+++ resources/js/functionAngular.js
@@ -1,4 +1,4 @@
- var app = angular.module('myApp',['ngMaterial']);
+ var app = angular.module('myApp',['ngRoute']);
  app.controller('MyController',  function($scope){
  	$scope.hienThi =false;
  	$scope.showEdit=function (){
@@ -6,3 +6,15 @@
  	}
 
  })
+ app.config(function ($routeProvider) {
+ 	var urlLocal="http://localhost:8080/PROJECT/testLRV/resources/views/";
+ 	$routeProvider.
+ 	when('/nvAdd',{
+ 		templateUrl:urlLocal+'admin/nhanVien_add.html'
+ 	})
+ 	.when('/nvEdit', {
+ 		templateUrl:urlLocal+'admin/nhanVien_edit.php'
+ 	})
+
+
+})
